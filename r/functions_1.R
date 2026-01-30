32ce7e1bedcb056072d872ad6d5d95dcfdbc301a
jsratke <jnsrtk@gmail.com>
2026-01-30 18:32:04 +0100

    Create functions_1.R
    Erste Vorlage für Funktionen-Skript 1 erstellt

---
+++ r/functions_1.R
@@ -0,0 +1,15 @@
+# (i)			deskriptive Statistiken (metrische Variablen)
+
+
+# (ii)		deskriptive Statistiken (kategoriale Variablen)
+
+
+# (iii)		deskriptive bivariate Statistiken (zwei kategoriale Variablen)
+
+
+# (iv)		deskriptive bivariate Statistiken (kategoriale & dichotome Variablen)
+
+
+# (v)			geeignete Visualisierung von drei/vier kategorialen Variablen
+
+
