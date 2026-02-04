bb9085ce82d9e644398edeab0a7c2355170eb17f
jsratke <jnsrtk@gmail.com>
2026-02-04 22:45:00 +0100

    Implement function ideas
    Added 2 functions with plan for possible statistics

--- r/functions_1.R
+++ r/functions_1.R
@@ -1,5 +1,18 @@
 # (i)			deskriptive Statistiken (metrische Variablen)
 
+# metr.stat - gibt deskriptive Statistiken für eine metrische Variable x aus
+metr.stat = function(x){
+
+  # einfacher Vektor mit Min, Max, Mean, Median, Quart, Sd
+
+
+
+  # Boxplot
+  
+
+  
+}
+
 
 # (ii)		deskriptive Statistiken (kategoriale Variablen)
 
@@ -8,6 +21,27 @@
 
 
 # (iv)		deskriptive bivariate Statistiken (kategoriale & dichotome Variablen)
+
+# biv_kat_dit.stat - gibt deskriptive Statistiken für Zusammenhang einer 
+#                    metrischen Variable x und einer dichotomen Variable y aus
+biv_kat_dit.stat = function(x, y){
+
+  # (Scatter-)Plot mit zwei versch. Farben
+
+
+
+
+  # Säulendiagramm mit doppelten Säulen
+
+
+  
+
+  # Zwei Boxplots nebeneinander
+
+
+  
+}
+
 
 
 # (v)			geeignete Visualisierung von drei/vier kategorialen Variablen
