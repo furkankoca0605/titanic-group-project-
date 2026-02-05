1af76e78e1e12f54ca85f32838c524b30505f309
furkankoca0605 <furkankoca0605@gmail.com>
2026-02-05 20:31:27 +0100

    weitere kommen noch

--- r/functions_2.R
+++ r/functions_2.R
@@ -1,1 +1,15 @@
 # interne Funktionen für functions_1.R
+
+(weitere kommem noch)
+
+get_var <- function(data, var) {
+  if (!is.data.frame(data)) stop("data muss ein data.frame sein.")
+  if (!is.character(var) || length(var) != 1) stop("var muss ein einzelner Spaltenname sein.")
+  if (!(var %in% names(data))) stop(paste0("Variable '", var, "' existiert nicht im Datensatz."))
+  data[[var]]
+}
+
+
+remove_na <- function(x) {
+  x[!is.na(x)]
+}
