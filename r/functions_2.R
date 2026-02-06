fd5896f12a3df6761bc9c08f5e7b2d9aab532dce
furkankoca0605 <furkankoca0605@gmail.com>
2026-02-06 14:29:58 +0100

    Add utility functions for data manipulation in R

--- r/functions_2.R
+++ r/functions_2.R
@@ -1,7 +1,8 @@
 # interne Funktionen für functions_1.R
 
-(weitere kommem noch)
 
+
+# 1) Spalte sicher aus Datensatz auslesen
 get_var <- function(data, var) {
   if (!is.data.frame(data)) stop("data muss ein data.frame sein.")
   if (!is.character(var) || length(var) != 1) stop("var muss ein einzelner Spaltenname sein.")
@@ -9,7 +10,40 @@ get_var <- function(data, var) {
   data[[var]]
 }
 
+# 2) Fehlende Werte entfernen (NA)
 
 remove_na <- function(x) {
   x[!is.na(x)]
 }
+
+# 3) Kategoriale Variablen bereinigen und faktorisieren
+
+
+
+clean_factor <- function(x) {
+  if (is.factor(x)) return(x)
+  x <- as.character(x)
+  x[x == ""] <- NA
+  factor(x)
+}
+
+# 4) Variablentyp erkennen (metrisch vs. kategorial)
+
+
+var_type <- function(x) {
+  if (is.numeric(x) || is.integer(x)) {
+    ux <- unique(x[!is.na(x)])
+    if (length(ux) > 2) return("metric")
+  }
+  return("categorical")
+}
+
+
+
+
+
+
+
+
+
+
