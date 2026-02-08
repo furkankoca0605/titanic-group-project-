0f27936709e0ef739acc88412330cd78f89f1437
jsratke <jnsrtk@gmail.com>
2026-02-08 17:35:54 +0100

    Add initial analysis structure in analysing.R

---
+++ r/analysing.R
@@ -0,0 +1,16 @@
+source("r/functions_1.R")
+# Anwenden der Funktionen aus functions_1.R 
+
+## Analyse metrische Variablen (z.B. Alter, Ticketpreis)
+
+
+## Analyse kategorielle Variablen (z.B. Klasse, Geschlecht, Einstiegshafen)
+
+
+## Vergleich kategoriell - kategoriell
+
+
+## Vergleich metrisch - dichotom (metrisch - kategoriell ?)
+
+
+## Vergleich mehrere kategorielle Variablen
