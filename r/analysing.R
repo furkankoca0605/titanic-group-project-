e8da48329d201ecf3496f34bf87adbd97a58e20b
jsratke <jnsrtk@gmail.com>
2026-02-08 19:06:13 +0100

    Analyse mancher Variablen eingefügt

--- r/analysing.R
+++ r/analysing.R
@@ -1,16 +1,27 @@
-source("r/functions_1.R")
 # Anwenden der Funktionen aus functions_1.R 
 
-## Analyse metrische Variablen (z.B. Alter, Ticketpreis)
+source("r/functions_1.R")
+titanic <- read.csv("data/titanic_clean.csv")
 
+## Analyse metrische Variablen 
+metr_stat(titanic, Age)
+metr_stat(titanic, Fare)
 
 ## Analyse kategorielle Variablen (z.B. Klasse, Geschlecht, Einstiegshafen)
 
 
-## Vergleich kategoriell - kategoriell
+## Vergleich kategoriell - kategoriell (z.B. Überlebt/Geschlecht, Überlebt/Deck, ...)
 
 
-## Vergleich metrisch - dichotom (metrisch - kategoriell ?)
-
+## Vergleich metrisch - dichotom
+met_dic_stat(titanic, Age, Survived)
+met_dic_stat(titanic, Fare, Survived)
 
 ## Vergleich mehrere kategorielle Variablen
+
+
+## Vergleich metrisch - kategoriell
+met_cat_stat(titanic, Fare, Embarked)
+met_cat_stat(titanic, Fare, Pclass)
+
+             
