5f0a0fc506000e87fda3fc16b5193e0be87d4643
Eldevir <n.roesener01@gmail.com>
2026-02-08 19:31:53 +0100

    Funktionen hinzugefügt
    Added frequency missing function and visualization plot for categorical variables.

--- r/analysing.R
+++ r/analysing.R
@@ -8,10 +8,12 @@ metr_stat(titanic, Age)
 metr_stat(titanic, Fare)
 
 ## Analyse kategorielle Variablen (z.B. Klasse, Geschlecht, Einstiegshafen)
-
+freq_missing(titanic,var)
 
 ## Vergleich kategoriell - kategoriell (z.B. Überlebt/Geschlecht, Überlebt/Deck, ...)
-
+#Survived als Variable vorgegeben und ist bereits eingegeben
+surv_wide(titanic, var())
+pearson_cat(titanic, var1, var2)
 
 ## Vergleich metrisch - dichotom
 met_dic_stat(titanic, Age, Survived)
@@ -24,4 +26,13 @@ met_dic_stat(titanic, Fare, Survived)
 met_cat_stat(titanic, Fare, Embarked)
 met_cat_stat(titanic, Fare, Pclass)
 
+##Zur Visualisierung
+#Anwendung mit Variablen
+plot_cats(
+  data = titanic,
+  x_var = "Pclass",
+  fill_var = "Survived",
+  facet_var = "Sex",
+  facet2_var = "Embarked"
+)
              
